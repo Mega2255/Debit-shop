f17e29cb8127b10eb47b2909e31db23cc7964337
Mega2255 <agbeishomadaniell@gmail.com>
2026-02-27 02:55:24 +0100

    Fix routing for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // <-- add this
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
